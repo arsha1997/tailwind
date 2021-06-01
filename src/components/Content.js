45256254e666fcb0e4a33d4fbdafd07f7cde8295
unknown <arshaskrd97@gmail.com>
2021-06-01 15:13:31 +0530

    use @ apply to create custom css

--- src/components/Content.js
+++ src/components/Content.js
@@ -5,25 +5,25 @@ import React from 'react';
 const Content = () => {
   return (
     <>
-      <div className="flex flex-col justify-center items-center bg-white h-screen font-mono py-40">
+      <div className="menu-card">
         <img
           src={ImageOne}
           alt="pizza"
           className="h-full rounded mb-20 shadow"
         />
-        <div className="flex flex-col justify-center items-center">
+        <div className="center-content">
           <h2 className="text-2xl mb-2">Pizza World</h2>
           <p className="mb-2">Crispy,Delicious,Nutritious</p>
           <span>$250</span>
         </div>
       </div>
-      <div className="flex flex-col justify-center items-center bg-white h-screen font-mono py-40">
+      <div className="menu-card">
         <img
           src={ImageTwo}
           alt="pizza"
           className="h-full rounded mb-20 shadow"
         />
-        <div className="flex flex-col justify-center items-center">
+        <div className="center-content">
           <h2 className="text-2xl mb-2">Chicken Pizza</h2>
           <p className="mb-2">Crispy,Delicious,Nutritious</p>
           <span>$200</span>
